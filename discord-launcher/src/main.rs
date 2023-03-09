8efd6ec4fba08fd757be7c6f81b4b7a5f1c3b9d9
DreamerChaserHAH <109950820+DreamerChaserHAH@users.noreply.github.com>
2023-03-10 01:56:47 +0800

    Added a functionality to detect user's current home directory, and launch discord application based on the user

--- discord-launcher/src/main.rs
+++ discord-launcher/src/main.rs
@@ -1,7 +1,13 @@
 //Importing Command
 use std::process::Command;
+//dynamically get user home folder
+use std::env;
 
 fn main() {
-    Command::new("C:\\Users\\WebRTC User\\AppData\\Local\\Discord\\Update.exe")
+
+    //println!("{:?}",env::home_dir());
+    let user_homedir = env::home_dir().unwrap().to_str().unwrap().to_string() + "\\AppData\\Local\\Discord\\Update.exe";
+
+    Command::new(user_homedir)
                 .args(["--processStart","Discord.exe"]).output().expect("failed to launch discord");
 }
